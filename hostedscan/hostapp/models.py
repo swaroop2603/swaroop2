4ddde8563f8f007ad7835084844722788d6f0d9d
swaroop2603 <84812732+swaroop2603@users.noreply.github.com>
2023-09-21 18:14:32 +0530

    Update models.py

--- hostedscan/hostapp/models.py
+++ hostedscan/hostapp/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Target(models.Model):
     
-    target = models.URLField(max_length=100)
+    target = models.CharField(max_length=100)
     tags = models.CharField(max_length=100)
 
     label = models.CharField(max_length=100)
@@ -16,20 +16,20 @@ class Target(models.Model):
         return self.Target
 class Scan(models.Model):
     
-    target = models.URLField(max_length=100)
+    target = models.CharField(max_length=100)
     pdf_file = models.FileField(upload_to='scans/pdfs/', null=True, blank=True)  # Store PDF file here
     label = models.CharField(max_length=100)
     date = models.DateTimeField(auto_now_add=True)
-    pdf_file = models.FileField(upload_to='pdfs/', null=True, blank=True)
+    
     json_file = models.FileField(upload_to='scans/jsons/', null=True, blank=True)  # Store JSON file here
     def __str__(self):
         return self.Scan
 class Risk(models.Model):
-    Title= models.CharField(max_length=100)
+    Title= models.CharField(max_length=100,default="")
     Scantype= models.CharField(max_length=100)
     target = models.URLField(max_length=100)
     Thread_Level= models.CharField(max_length=100)
-    openvas_QOD=models.FloatField()
+    
     STatus= models.CharField(max_length=100)
     
     date = models.DateTimeField(auto_now_add=True)
