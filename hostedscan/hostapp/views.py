d7b52193595672f3a7e2653be9036ad0c155fd74
swaroop2603 <84812732+swaroop2603@users.noreply.github.com>
2023-09-21 18:14:58 +0530

    Update views.py

--- hostedscan/hostapp/views.py
+++ hostedscan/hostapp/views.py
@@ -13,11 +13,29 @@ from rest_framework.response import Response
 from django.http import FileResponse
 from .pagination import CustomPagination
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.generics import GenericAPIView
-
+from rest_framework.generics import GenericAPIView,RetrieveAPIView
+from uuid import uuid4
+import re
+from urllib.parse import urlparse
+from django.db.models import F, Value
 class TargetAPI(viewsets.ModelViewSet):       # add this
   serializer_class = Targetserializer         # add this
   queryset = Target.objects.all()
+def is_valid_target(value):
+    # Define regular expressions for IPv4, domain, and public CIDR formats
+    ipv4_pattern = re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')
+    domain_pattern = re.compile(r'^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
+    public_cidr_pattern = re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2}$')
+
+    # Check if the value matches any of the patterns
+    if ipv4_pattern.match(value) or public_cidr_pattern.match(value):
+        return True
+    parsed_url = urlparse(value)
+    domain = parsed_url.netloc
+    if domain_pattern.match(domain):
+        return True
+
+    return False
 class ApiView(GenericAPIView):
     pagination_class = PageNumberPagination  # Use the PageNumberPagination class
     page_size = 10
@@ -25,10 +43,19 @@ class ApiView(GenericAPIView):
     queryset = Target.objects.all()
     pagination_class = CustomPagination
     def post(self, request):
-        serializer=Targetserializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-           serializer.save()
-           return Response(serializer.data)
+        target_url=request.data.get("target")
+        print(f"{target_url}")
+        if not is_valid_target(target_url):
+            print(f"Invalid target format: {target_url}")
+            return Response({"error": "Invalid target format"}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            serializer = Targetserializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                print(f"Serializer errors: {serializer.errors}")
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
       # Use the custom pagination class
 
     def get_object(self, id):
@@ -45,17 +72,23 @@ class ApiView(GenericAPIView):
                 return Response(serializer.data)
             return Response({"detail": "Target not found."}, status=status.HTTP_404_NOT_FOUND)
         else:
-            queryset = self.get_queryset()
-            page = self.paginate_queryset(queryset)
+            sort_field = request.query_params.get('sortField', 'date')
+            sort_order = request.query_params.get('sortOrder', 'desc')
+            print(f"{sort_field}")
 
+        if sort_order == 'desc':
+            queryset = Target.objects.all().order_by(F(sort_field).desc())
+        else:
+            queryset = Target.objects.all().order_by(F(sort_field).desc())
+
+            page = self.paginate_queryset(queryset)
             if page is not None:
                 serializer = Targetserializer(page, many=True)
                 return self.get_paginated_response(serializer.data)
 
-            # If there is no pagination, return all data
-            targets = Target.objects.all()
-            serializer = Targetserializer(targets, many=True)
+            serializer =Targetserializer(page, many=True)
             return Response(serializer.data)
+            
 
     def put(self, request,id=None):  # Change 'id' to 'pk'
         if id is not None:
@@ -87,17 +120,34 @@ class ApiView_s(GenericAPIView):
     queryset = Scan.objects.all()
     pagination_class = CustomPagination
     def post(self, request):
+        print(f"\n\n****post****")
+        #target_url = "google.com"
+        target_url = request.data.get('target')
+        target_url_main=target_url
+        labels=request.data.get('label')
+        print(f"\n\n****{target_url}****")
+        ipv4_pattern = re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')
+        domain_pattern = re.compile(r'^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
+        public_cidr_pattern = re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2}$')
 
-        target_url = "google.com"
-        #target_url = request.data.get('target')
-        
-        #if not target_url:
-        #   return JsonResponse({'detail': 'Target URL is required in the request data'}, status=status.HTTP_400_BAD_REQUEST)
+        # Check if the value matches any of the patterns
+        if ipv4_pattern.match(target_url) or public_cidr_pattern.match(target_url):
+            target_url=target_url
+        parsed_url = urlparse(target_url)
+        domain = parsed_url.netloc
+        if domain_pattern.match(domain):
+            target_url=domain
+        print(f"\n\n****{target_url}****")
+        if not target_url:
+            print(f"\n\n****if post****")
+            return JsonResponse({'detail': 'Target URL is required in the request data'}, status=status.HTTP_400_BAD_REQUEST)
         try:
+            print(f"\n\n****try post****")
             all_scan_requests = [
                 ServerScanRequest(server_location=ServerNetworkLocation(hostname=target_url)),
             ]
         except ServerHostnameCouldNotBeResolved:
+            print(f"\n\n****expect post****")
             return JsonResponse({'detail': 'Error resolving the supplied hostname'}, status=status.HTTP_400_BAD_REQUEST)
 
         scanner = Scanner()
@@ -143,24 +193,99 @@ class ApiView_s(GenericAPIView):
         print(f"\n\n=> Saving scan results to out infun")
         # Return a JSON response with the scan results or a success message
         pdf_content_file = ContentFile(pdf_content, name='report.pdf')
+        print(f"\n\n=> if1")
         serializer = Scanserializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            
-            target = request.data.get('target')
-            label = request.data.get('label')
-            
-        else:
-            return Response({'detail': 'Invalid data'}, status=status.HTTP_400_BAD_REQUEST)
+        print(f"\n\n=> if2")
+        unique_filename = f"{uuid4()}.pdf"
+
+        # Save the PDF content with the unique filename
+        pdf_file = ContentFile(pdf_content,name='report.pdf')
+
 
         # Create a dictionary to hold all data
         scan = Scan(
-            target=target,
-            pdf_file=pdf_content_file,
+            target=target_url_main,
+            pdf_file=pdf_file,
             json_file=json_content_file,
-            label=label,
+            label=labels,
             
         )
         scan.save()
+        def get_risks(scan_results):
+            first_scan_result = scan_results[0].scan_result
+            
+            risks_list = [["Name", "Description", "Severity", "Status"]]
+            title=[]
+            threadlvl=[]
+            status=[]
+            url=[]
+            scantype=[]
+            risktls13name = "%s -> %s" % (first_scan_result.tls_1_3_cipher_suites.result.tls_version_used.name, 
+                                        first_scan_result.tls_1_3_cipher_suites.result.is_tls_version_supported)
+            risks_list.append([risktls13name, "TLS 1.3 vulnerability", "Critical", "Open"])
+            
+            print(f"{title}")
+            title.append(risks_list[1][1])
+            threadlvl.append(risks_list[1][2])
+            status.append(risks_list[1][3])
+            url.append(target_url)
+            scantype.append("ssylze")
+                
+            risktls12name = "%s -> %s" % (first_scan_result.tls_1_2_cipher_suites.result.tls_version_used.name, 
+                                        first_scan_result.tls_1_2_cipher_suites.result.is_tls_version_supported)
+            risks_list.append([risktls12name, "TLS 1.2 generic", "High", "Open"])
+        
+            print(f"{title}")
+            title.append(risks_list[2][1])
+            threadlvl.append(risks_list[2][2])
+            status.append(risks_list[2][3])
+            url.append(target_url)
+            scantype.append("ssylze")
+                
+            risktls11name = "%s -> %s" % (first_scan_result.tls_1_1_cipher_suites.result.tls_version_used.name, 
+                                        first_scan_result.tls_1_1_cipher_suites.result.is_tls_version_supported)
+            risks_list.append([risktls11name, "TLS 1.1 support", "Medium", "Open"])
+            
+            print(f"{title}")
+            title.append(risks_list[3][1])
+            threadlvl.append(risks_list[3][2])
+            status.append(risks_list[3][3])
+            url.append(target_url)
+            scantype.append("ssylze")
+                
+            risktls10name = "%s -> %s" % (first_scan_result.tls_1_0_cipher_suites.result.tls_version_used.name, 
+                                        first_scan_result.tls_1_0_cipher_suites.result.is_tls_version_supported)
+            risks_list.append([risktls10name, "TLS 1.0 related", "Low", "Open"])
+            
+            print(f"{title}")
+            title.append(risks_list[4][1])
+            threadlvl.append(risks_list[4][2])
+            status.append(risks_list[4][3])
+            url.append(target_url)
+            scantype.append("ssylze")
+            rejected_cipher_suitessslv3 = first_scan_result.ssl_3_0_cipher_suites.result.rejected_cipher_suites
+            for i in rejected_cipher_suitessslv3:
+                sslv3risk_name = "%s->%s" %(i.cipher_suite.name,i.cipher_suite.openssl_name)
+                risks_list.append([sslv3risk_name, "SSL V3 detail", "Info", "N/A"])
+                break
+            title.append(risks_list[5][1])
+            threadlvl.append(risks_list[5][2])
+            status.append(risks_list[5][3])
+            url.append(target_url)
+            scantype.append("ssylze")
+            print(f"{title}")
+            risk5=Risk(
+                    Title=list(title),
+                    Scantype=scantype,
+                    target =url,
+                    Thread_Level=threadlvl,
+                    STatus=status,
+                )
+            risk5.save()
+            print(f"{title}")
+            print(f"{risks_list}")
+            return risks_list
+        risks_list = get_risks(all_server_scan_results)
         return JsonResponse({'detail': 'Scan completed successfully'}, status=status.HTTP_201_CREATED)
 
     def example_json_result_output(self, json_file_out, all_server_scan_results, date_scans_started, date_scans_completed):
@@ -215,6 +340,7 @@ class ApiView_s(GenericAPIView):
         except Scan.DoesNotExist:
             return None
     def get(self, request,id=None):
+        
         if id is not None:
             try:
                 target = Scan.objects.get(pk=id)
@@ -223,15 +349,22 @@ class ApiView_s(GenericAPIView):
             except Scan.DoesNotExist:
                 return Response({"detail": "Target not found."}, status=status.HTTP_404_NOT_FOUND)
         else:
-            queryset = self.get_queryset()
+            
+            sort_field = request.query_params.get('sortField', 'date')
+            sort_order = request.query_params.get('sortOrder', 'desc')
+            print(f"{sort_field}")
+
+        if sort_order == 'desc':
+            queryset = Scan.objects.all().order_by(F(sort_field).desc())
+        else:
+            queryset = Scan.objects.all().order_by(F(sort_field).desc())
+
             page = self.paginate_queryset(queryset)
             if page is not None:
                 serializer = Scanserializer(page, many=True)
                 return self.get_paginated_response(serializer.data)
 
-            # If there is no pagination, return all data
-            targets = Scan.objects.all()
-            serializer = Scanserializer(targets, many=True)
+            serializer = Scanserializer(page, many=True)
             return Response(serializer.data)
     def delete(self, request, id=None):
             if id is not None:
@@ -243,7 +376,45 @@ class ApiView_s(GenericAPIView):
                     return Response({"detail": "Target not found."}, status=status.HTTP_404_NOT_FOUND)
             else:
                 return Response({"detail": "Invalid request."}, status=status.HTTP_400_BAD_REQUEST)
-            
+class she(GenericAPIView):
+    print(f"\n\n=> scheduling")
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+from apscheduler.jobstores.base import JobLookupError
+
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+class Apiview_scan_schedule(GenericAPIView):
+    def post(self,request):
+        target_url = request.data.get('target')
+        labels = request.data.get('label')
+        scan_frequency=3600*24
+        if not target_url:
+            return JsonResponse({'detail': 'Target URL is required in the request data'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Generate a unique job ID based on the target_url
+        job_id = f'scan_{target_url.replace("/", "_")}' 
+        try:
+            # Check if a job with the same ID exists
+            existing_job = scheduler.get_job(job_id)
+            if existing_job:
+                # Reschedule the existing job with the new parameters
+                existing_job.modify(trigger=IntervalTrigger(seconds=scan_frequency), args=[target_url, labels])
+            else:
+                # Create a new job with the unique job ID
+                scheduler.add_job(
+                    self.schedule_scan,
+                    IntervalTrigger(seconds=scan_frequency),  # Schedule the scan at the specified frequency
+                    args=[target_url, labels],
+                    id=job_id,  # Use the unique job ID
+                )
+        except JobLookupError:
+            return JsonResponse({'detail': 'Error looking up the job'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        return JsonResponse({'detail': 'Scan scheduled successfully'}, status=status.HTTP_202_ACCEPTED)
+
+
 class ApiView_Risk(GenericAPIView):
     pagination_class = PageNumberPagination  # Use the PageNumberPagination class
     page_size = 10
@@ -269,18 +440,46 @@ class ApiView_Risk(GenericAPIView):
             except Scan.DoesNotExist:
                 return Response({"detail": "Target not found."}, status=status.HTTP_404_NOT_FOUND)
         else:
-            queryset = self.get_queryset()
-            page = self.paginate_queryset(queryset)
+            sort_field = request.query_params.get('sortField', 'date')
+            sort_order = request.query_params.get('sortOrder', 'desc')
+            print(f"{sort_field}")
 
+        if sort_order == 'desc':
+            queryset = Risk.objects.all().order_by(F(sort_field).desc())
+        else:
+            queryset = Risk.objects.all().order_by(F(sort_field).desc())
+
+            page = self.paginate_queryset(queryset)
             if page is not None:
                 serializer = Riskserializer(page, many=True)
                 return self.get_paginated_response(serializer.data)
 
-            # If there is no pagination, return all data
-            targets = Risk.objects.all()
-            serializer = Riskserializer(targets, many=True)
+            serializer = Riskserializer(page, many=True)
             return Response(serializer.data)
-            
+    def delete(self, request, id=None):
+            if id is not None:
+                try:
+                    target = Risk.objects.get(pk=id)
+                    target.delete()  # Delete the target
+                    return Response({"detail": "Target deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+                except Scan.DoesNotExist:
+                    return Response({"detail": "Target not found."}, status=status.HTTP_404_NOT_FOUND)
+            else:
+                return Response({"detail": "Invalid request."}, status=status.HTTP_400_BAD_REQUEST)
+class Viewpdf(RetrieveAPIView):
+    def get_object(self, id):
+        try:
+            return Scan.objects.get(pk=id)
+        except Scan.DoesNotExist:
+            return None
+    def get(self, request, id=None):
+        if id is not None:
+            scan = Scan.objects.get(pk=id)
+            pdf_file = scan.pdf_file
+            response = FileResponse(pdf_file, content_type='application/pdf')
+            response['Content-Disposition'] = f'attachment; filename="{pdf_file.name}"'
+            return response
+    
 # views.py
 
 # views.py
